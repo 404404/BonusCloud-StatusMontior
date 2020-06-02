c1fa09f2c253c2bf971769d28c5809c498059632
404404 <32157663+404404@users.noreply.github.com>
2020-06-02 16:10:09 +0800

    Update the layout and add the NVMe support

--- x86-x64/status-pm.sh
+++ x86-x64/status-pm.sh
@@ -3,10 +3,6 @@
 # Base on: https://github.com/BonusCloud/BonusCloud-Node
 # Special thanks to qinghon https://github.com/qinghon
 
-OS=""
-OS_CODENAME=""
-PG=""
-
 echoerr() { 
     printf "\033[1;31m$1\033[0m" 
 }
@@ -23,65 +19,34 @@ echorun() {
     esac
 }
 
-sys_osname(){
-    if  which lsb_release >/dev/null  2>&1; then
-        OS=$(lsb_release -is|tr '[A-Z]' '[a-z]')
-        OS_CODENAME=$(lsb_release -cs|tr '[A-Z]' '[a-z]')
-        return 
-    fi
-    source /etc/os-release
-    case $ID in
-        ubuntu )
-            OS="Ubuntu" 
-            ;;
-        debian ) 
-            OS="Debian"
-            ;;
-        raspbian )
-            OS="Raspbian"
-            ;;
-        centos ) OS="Centos" ;;
-        *       ) OS="$ID"
-    esac
-}
+#获取系统盘符，应对系统盘不是sda的情况
+root_type=$(lsblk -ar 2>>/dev/null | grep -w "/" | awk '{print $(NF-1)}')
+root_name=$(lsblk -ar 2>>/dev/null | grep -w "/" | awk '{print $1}')
+root_disk=""
+if [ x"${root_type}" == x"lvm" ];then
+    root_vg_name=$(echo ${root_name} | awk -F- '{print $1}')
+    root_disk=$(pvs 2>>/dev/null | grep ${root_vg_name} | awk '{print $1}' | sed 's/[0-9]//;s/\/dev\///')
+else
+    root_disk=$(lsblk -ar 2>>/dev/null | grep -w "/" | awk '{print $1}' | sed 's/[0-9]//')
+fi
 
-_check_pg(){
-    # Detection package manager
-    if which apt >/dev/null 2>&1 ; then
-        # echoinfo "Find apt\n"
-        PG="apt"
-    elif which yum >/dev/null 2>&1 ; then
-        # echoinfo "Find yum\n"
-        PG="yum"
-    elif which pacman>/dev/null 2>&1 ; then
-        # log "[info]" "Find pacman"
-        PG="pacman"
-    else
-        log "[error]" "\"apt\" or \"yum\" ,not found ,exit "
-        exit 1
-    fi
-}
+cat /root/version.txt
 
-smarttool_ins(){
-    if which smartctl  >/dev/null 2>&1; then
-        return
-    fi
-    case $PG in
-        apt|yum ) $PG install smartmontools -y ;;
-        pacman ) $PG --needed --noconfirm -S smartmontools ;;
-    esac
-}
+echowarn "\n\nCPU温度 / CPU temperature: \t"
+T3=$(cat /sys/class/thermal/thermal_zone0/temp | awk '{print int($1/1000)}')
+if [[ ${T3} < 65 ]]; then  
+    echoinfo "${T3}°C"
+else
+    echoerr "${T3}°C"
+fi
 
-smarttool_ins
+echowarn "\n总缓存空间 / Total cache space: "
+vgs_have=$(vgs | grep -q 'BonusVolGroup' ;echo $?)
+free_space=$(vgdisplay | grep 'VG Size' | awk '{print $3,$4}' | sed -r 's/\i//g')
+[[ ${vgs_have} -eq 0  ]] && echoinfo "${free_space} \t "
+[[ ${vgs_have} -eq 1  ]] && echowarn "运行hdd.sh后重试 / run hdd.sh and try again \t "
 
-sys_osname
-printf "系统名称 / System name：  ${OS}"
-
-echowarn "\n运行进程 / Progress:      "
 lvm_have=$(lvs 2>/dev/null | grep -q 'BonusVolGroup';echo $?)
-vg_have=$(vgs 2>/dev/null | grep -q 'BonusVolGroup';echo $?)
-# df_have=$(df -BG 2> /dev/null| grep -q "/dev/mapper/BonusVolGroup-bonusvol";echo $?)
-
 [[ ${lvm_have} -eq 0  ]] && { echorun "0";}|| echorun "1"
 printf "\n"
 
@@ -106,40 +71,36 @@ for lv in $lvlist; do
     printf "─────────────────────────────────────────────────────────────────────\n"
 done
 
-echowarn "\n中央处理器温度 / CPU temperature:   "
-T3=$(cat /sys/class/thermal/thermal_zone0/temp | awk '{print int($1/1000)}')
-if [[ ${T3} < 65 ]]; then  
-    echoinfo "${T3}°C"
-else
-    echoerr "${T3}°C"
-fi
-
-echowarn "\n数据盘容量总和 / Capacity of disks: "
-free_space=$(vgdisplay | grep 'VG Size' | awk '{print $3,$4}' | sed -r 's/\i//g')
-[[ ${vg_have} -eq 0  ]] && echoinfo "${free_space}\n"
-[[ ${vg_have} -eq 1  ]] && echowarn "VG未创建 / VG is not created \n"
-
-for sd in $(ls /dev/*|grep -E '((sd)|(vd)|(hd))[a-z]$'); do
+for sd in $(ls /dev/* | grep -E '((sd)|(vd)|(hd)|(nvme))[a-z]$' | grep -v "${root_disk}" ); do
     smartinfo=$(smartctl -d sat -a "${sd}")
+    pv_have=$(pvs 2>/dev/null | grep -q "${sd}" ;echo $?)
+    vg_have=$(pvs 2>/dev/null | grep "${sd}" | grep -q "BonusVolGroup";echo $?)
     I1=$(echo "$smartinfo" | grep 'User Capacity' | awk '{print $5 $6}' | sed -r 's#\[##g' | sed -r 's#\]##g')
-    T1=$(echo "$smartinfo"| grep Temperature_Celsius | awk '{print $10}')
-    [[ ${vg_have} -eq 0  ]] && C1=$(pvs | grep "${sd}" | awk '{print ($5 - $6)}' | sed -r 's#g##g')
-    [[ ${vg_have} -eq 0  ]] && C2=$(pvs | grep "${sd}" | awk '{print $6}' | sed -r 's#.[0-9]{2}g##g')
+    T1=$(echo "$smartinfo"| grep '194 Temperature_Celsius' | awk '{print $10}')
 
-    echoinfo "${sd}    "
+    echoinfo "${sd}     "
     if [[ ${T1} < 60 ]]; then 
-        echoinfo "${T1}°C\t"
+        echoinfo "${T1}°C     "
     else
-        echoerr "${T1}°C\t"
+        echoerr "${T1}°C     "
     fi
-    [[ ${vg_have} -eq 0  ]] && printf "${C1}GB / ${C2}GB / "
-    [[ ${vg_have} -eq 1  ]] && printf "0GB / 0GB / "
-    printf "${I1} \t\t"
-    smart_test=$(echo "$smartinfo" | grep 'SMART overall-health self-assessment test result' | awk '{print $6}')
-    if [[ ${smart_test} == "PASSED" ]]; then 
-        echoinfo "通过/Passed\n"
+
+    [[ ${pv_have} -eq 0 ]] && C1=$(lsblk ${sd} | sed '1,2d' | awk 'BEGIN {sum = 0} {sum += $4} END {print sum}')
+    [[ ${pv_have} -eq 0 ]] && C2=$(pvs 2>/dev/null | grep "${sd}" | awk '{print $6}' | sed -r 's/\.[0-9][0-9]//g' | tr 'a-z' 'A-Z')
+    if [[ ${pv_have} == 0 ]]; then 
+        if [[ ${C1} > 999 ]]; then
+            C1=$(${C1} / 1000)
+            [[ ${vg_have} -eq 0 ]] && printf " ${C1}TB / ${C2}B "
+        else  
+            [[ ${vg_have} -eq 0 ]] && printf " ${C1}GB / ${C2}B "
+        fi
     else
-        echoerr "警告/Waning\n"
-    fi 
+        echoerr "  --- "
+        printf "/" && echoerr " --- "
+    fi
+    [[ ${vg_have} -eq 1 ]] && C3=$(fdisk -l | grep "${sd}" | awk '{print int($3) $4}' | sed 's/\i//g' | sed 's/\,//g')
+    [[ ${pv_have} -eq 0 && ${vg_have} -eq 1 ]] && echoerr "  --- " && printf "/ ${C3} "
+    printf "/ ${I1}\n"
+
     lsblk -b $sd | awk '{print $1}' | sed -r 's#BonusVolGroup-bonusvol([A-Za-z0-9])#\1#g' | sed 1,2d
 done
--- x86-x64/status-vm.sh
+++ x86-x64/status-vm.sh
@@ -3,10 +3,6 @@
 # Base on: https://github.com/BonusCloud/BonusCloud-Node
 # Special thanks to qinghon https://github.com/qinghon
 
-OS=""
-OS_CODENAME=""
-PG=""
-
 echoerr() { 
     printf "\033[1;31m$1\033[0m" 
 }
@@ -22,60 +18,6 @@ echorun() {
         "0" ) echoinfo "已调度 / Running\t";;
     esac
 }
-
-sys_osname(){
-    if  which lsb_release >/dev/null  2>&1; then
-        OS=$(lsb_release -is|tr '[A-Z]' '[a-z]')
-        OS_CODENAME=$(lsb_release -cs|tr '[A-Z]' '[a-z]')
-        return 
-    fi
-    source /etc/os-release
-    case $ID in
-        ubuntu )
-            OS="Ubuntu" 
-            ;;
-        debian ) 
-            OS="Debian"
-            ;;
-        raspbian )
-            OS="Raspbian"
-            ;;
-        centos ) OS="Centos" ;;
-        *       ) OS="$ID"
-    esac
-}
-
-_check_pg(){
-    # Detection package manager
-    if which apt >/dev/null 2>&1 ; then
-        # echoinfo "Find apt\n"
-        PG="apt"
-    elif which yum >/dev/null 2>&1 ; then
-        # echoinfo "Find yum\n"
-        PG="yum"
-    elif which pacman>/dev/null 2>&1 ; then
-        # log "[info]" "Find pacman"
-        PG="pacman"
-    else
-        log "[error]" "\"apt\" or \"yum\" ,not found ,exit "
-        exit 1
-    fi
-}
-
-smarttool_ins(){
-    if which smartctl  >/dev/null 2>&1; then
-        return
-    fi
-    case $PG in
-        apt|yum ) $PG install smartmontools -y ;;
-        pacman ) $PG --needed --noconfirm -S smartmontools ;;
-    esac
-}
-
-smarttool_ins
-
-sys_osname
-printf "系统名称 / System name：  ${OS}"
 
 echowarn "\n运行进程 / Progress:      "
 lvm_have=$(lvs 2>/dev/null | grep -q 'BonusVolGroup';echo $?)
@@ -107,4 +49,31 @@ for lv in $lvlist; do
     echoinfo " ${TYPE}-${lvm_num}-${lvm_size}GB \n"
     echo -e "$(df -h |grep "bonusvol$lv" | awk '{print " ├─", $1, "\t\t", $3, "\t\t", $4, "\t\t   ", $5}' | sed -r 's#/dev/mapper/BonusVolGroup-bonusvol([A-Za-z0-9])#\1#g')"
     printf "─────────────────────────────────────────────────────────────────────\n"
+done
+
+for sd in $(ls /dev/*|grep -E '((sd)|(vd)|(hd))[a-z]$'); do
+    smartinfo=$(smartctl -d sat -a "${sd}")
+    I1=$(echo "$smartinfo" | grep 'User Capacity' | awk '{print $5 $6}' | sed -r 's#\[##g' | sed -r 's#\]##g')
+    pv_have=$(pvs 2>/dev/null | grep -q "${sd}" ;echo $?)
+    vg_have=$(pvs 2>/dev/null | grep "${sd}" | grep -q "BonusVolGroup";echo $?)
+
+    echoinfo "${sd}     "
+    [[ ${pv_have} -eq 0 ]] && C1=$(lsblk ${sd} | sed '1,2d' | awk 'BEGIN {sum = 0} {sum += $4} END {print sum}')
+    [[ ${pv_have} -eq 0 ]] && C2=$(pvs 2>/dev/null | grep "${sd}" | awk '{print $6}' | sed -r 's/\.[0-9][0-9]//g' | tr 'a-z' 'A-Z')
+    if [[ ${pv_have} == 0 ]]; then 
+        if [[ ${C1} > 999 ]]; then
+            C1=$(${C1} / 1000)
+            [[ ${vg_have} -eq 0 ]] && printf " ${C1}TB / ${C2}B "
+        else  
+            [[ ${vg_have} -eq 0 ]] && printf " ${C1}GB / ${C2}B "
+        fi
+    else
+        echoerr "  --- "
+        printf "/" && echoerr " --- "
+    fi
+    [[ ${vg_have} -eq 1 ]] && C3=$(fdisk -l | grep "${sd}" | awk '{print int($3) $4}' | sed 's/\i//g' | sed 's/\,//g')
+    [[ ${pv_have} -eq 0 && ${vg_have} -eq 1 ]] && echoerr "  --- " && printf "/ ${C3} "
+    printf "/ ${I1}\n"
+
+    lsblk -b $sd | awk '{print $1}' | sed -r 's#BonusVolGroup-bonusvol([A-Za-z0-9])#\1#g' | sed 1,2d
 done
