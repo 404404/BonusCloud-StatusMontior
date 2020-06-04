cf0ea4947bb1ac5b9a708f9c1217e0a9418adac3
404404 <32157663+404404@users.noreply.github.com>
2020-06-04 23:03:25 +0800

    Change the layout
    Change the layout
    Determine the image version by reading system Settings and environments instead of reading static files

--- x86-x64/status-vm.sh
+++ x86-x64/status-vm.sh
@@ -18,8 +18,53 @@ echorun() {
         "0" ) echoinfo "已调度 / Running\t";;
     esac
 }
+sys_osname(){
+    if  which lsb_release >/dev/null  2>&1; then
+        OS=$(lsb_release -is)
+        OS_CODENAME=$(lsb_release -cs)
+        return 
+    fi
+    source /etc/os-release
+    case $ID in
+        Debian ) 
+            OS="Debian 9"
+            if [[ $VERSION_CODENAME != "" ]]; then
+                OS_CODENAME=$VERSION_CODENAME
+            else
+                OS_CODENAME=$(echo "$VERSION"|sed -e 's/(//g' -e 's/)//g'|awk '{print $2}')
+            fi
+            ;;
+        Centos ) OS="Centos 7.6" ;;
+        *       ) OS="$ID"
 
-echowarn "\n运行进程 / Progress:      "
+    esac
+}
+
+#获取系统盘符，应对系统盘不是sda的情况
+root_type=$(lsblk -ar 2>>/dev/null | grep -w "/" | awk '{print $(NF-1)}')
+root_name=$(lsblk -ar 2>>/dev/null | grep -w "/" | awk '{print $1}')
+root_disk=""
+if [ x"${root_type}" == x"lvm" ];then
+    root_vg_name=$(echo ${root_name} | awk -F- '{print $1}')
+    root_disk=$(pvs 2>>/dev/null | grep ${root_vg_name} | awk '{print $1}' | sed 's/[0-9]//;s/\/dev\///')
+else
+    root_disk=$(lsblk -ar 2>>/dev/null | grep -w "/" | awk '{print $1}' | sed 's/[0-9]//')
+fi
+
+sys_osname
+printf "OS name: ${OS}     Type: "
+disk_support=$(cat /lib/systemd/system/bxc-node.service | grep -q 'devoff' ;echo $?)
+if [[ ${disk_support} == 0 ]]; then
+    printf "Dual     "
+else 
+    printf "Single     "
+fi
+# 每次版本更新修改这里
+printf "Release date: "
+printf "2020.06.04 \n"
+# cat /root/version.txt
+
+echowarn "运行进程 / Progress:      "
 lvm_have=$(lvs 2>/dev/null | grep -q 'BonusVolGroup';echo $?)
 vg_have=$(vgs 2>/dev/null | grep -q 'BonusVolGroup';echo $?)
 
@@ -35,8 +80,8 @@ declare -A dict
 # 任务类型字典
 dict=([iqiyi]="A" [yunduan]="B" [65542v]="C" [65541v]="D" [65540v]="F")
 
-[[ ${lvm_have} -eq 0  ]] && lvs_info=$(lvs 2>/dev/null|grep BonusVolGroup|grep bonusvol)
-[[ ${lvm_have} -eq 0  ]] && lvlist=$(echo "$lvs_info"|awk '{print $1}'|sed -r 's#bonusvol([A-Za-z0-9]+)[0-9]{2}#\1#g'|sort -ru -k 1.4)
+[[ ${lvm_have} -eq 0  ]] && lvs_info=$(lvs 2>/dev/null | grep BonusVolGroup | grep bonusvol)
+[[ ${lvm_have} -eq 0  ]] && lvlist=$(echo "$lvs_info" | awk '{print $1}' | sed -r 's#bonusvol([A-Za-z0-9]+)[0-9]{2}#\1#g' | sort -ru -k 1.4)
 [[ ${lvm_have} -eq 0  ]] && printf "─────────────────────────────────────────────────────────────────────\n"
 [[ ${lvm_have} -eq 0  ]] && echowarn " 任务属性\t\t 已使用\t\t 可用 \t\t  已用百分比\n"
 [[ ${lvm_have} -eq 0  ]] && echowarn "  Type   \t\t  Used \t\t Avail\t\t   Used%%  \n"
@@ -44,22 +89,33 @@ dict=([iqiyi]="A" [yunduan]="B" [65542v]="C" [65541v]="D" [65540v]="F")
 
 for lv in $lvlist; do
     TYPE=${dict[$lv]}
-    lvm_num=$(echo "$lvs_info"|awk '{print $1}'|grep -c "$lv")
-    lvm_size=$(echo "$lvs_info"|grep "$lv"|awk '{print $4}'|head -n 1|sed 's/\.00g//g')
+    lvm_num=$(echo "$lvs_info" | awk '{print $1}' | grep -c "$lv")
+    lvm_size=$(echo "$lvs_info" | grep "$lv" | awk '{print $4}' | head -n 1 | sed 's/\.00g//g')
     echoinfo " ${TYPE}-${lvm_num}-${lvm_size}GB \n"
-    echo -e "$(df -h |grep "bonusvol$lv" | awk '{print " ├─", $1, "\t\t", $3, "\t\t", $4, "\t\t   ", $5}' | sed -r 's#/dev/mapper/BonusVolGroup-bonusvol([A-Za-z0-9])#\1#g')"
+    echo -e "$(df -h | grep "bonusvol$lv" | awk '{print " ├─", $1, "\t\t", $3, "\t\t", $4, "\t\t   ", $5}' | sed -r 's#/dev/mapper/BonusVolGroup-bonusvol([A-Za-z0-9])#\1#g')"
     printf "─────────────────────────────────────────────────────────────────────\n"
 done
 
-for sd in $(ls /dev/*|grep -E '((sd)|(vd)|(hd))[a-z]$'); do
+root_disks=$(ls /dev/* | grep "${root_disk}" | sed -n 1p)
+smarts=$(smartctl -d sat -a "${root_disks}")
+echoinfo "${root_disks}     "
+I0=$(echo "$smarts" | grep 'User Capacity' | awk '{print $5 $6}' | sed -r 's#\[##g' | sed -r 's#\]##g')
+printf "${I0}\n"
+if [[ ${OS} == "centos" && ${disk_support} == 0 ]]; then
+    lsblk "${root_disks}" | awk '{print $1}'| sed -r 's#BonusVolGroup-bonusvol([A-Za-z0-9])#\1#g' | sed 1,2d 
+fi
+
+for sd in $(ls /dev/* | grep -E '((sd)|(vd)|(hd)|(nvme))[a-z]$' | grep -v "${root_disk}" ); do
     smartinfo=$(smartctl -d sat -a "${sd}")
-    I1=$(echo "$smartinfo" | grep 'User Capacity' | awk '{print $5 $6}' | sed -r 's#\[##g' | sed -r 's#\]##g')
     pv_have=$(pvs 2>/dev/null | grep -q "${sd}" ;echo $?)
-    vg_have=$(pvs 2>/dev/null | grep "${sd}" | grep -q "BonusVolGroup";echo $?)
+    vg_have=$(pvs 2>/dev/null | grep "${sd}" | grep -q "BonusVolGroup" ;echo $?)
+    temp_have=$(echo smartctl -d sat -a "${sd}" | grep -q "194 Temperature_Celsius" ;echo $?)
+    I1=$(echo "$smartinfo" | grep 'User Capacity' | awk '{print $5 $6}' | sed -r 's#\[##g' | sed -r 's#\]##g')
 
     echoinfo "${sd}     "
     [[ ${pv_have} -eq 0 ]] && C1=$(lsblk ${sd} | sed '1,2d' | awk 'BEGIN {sum = 0} {sum += $4} END {print sum}')
     [[ ${pv_have} -eq 0 ]] && C2=$(pvs 2>/dev/null | grep "${sd}" | awk '{print $6}' | sed -r 's/\.[0-9][0-9]//g' | tr 'a-z' 'A-Z')
+    printf "${I1} - "
     if [[ ${pv_have} == 0 ]]; then 
         if [[ ${C1} > 999 ]]; then
             C1=$(${C1} / 1000)
@@ -69,11 +125,12 @@ for sd in $(ls /dev/*|grep -E '((sd)|(vd)|(hd))[a-z]$'); do
         fi
     else
         echoerr "  --- "
-        printf "/" && echoerr " --- "
+        printf "/"
+        echoerr " --- "
     fi
     [[ ${vg_have} -eq 1 ]] && C3=$(fdisk -l | grep "${sd}" | awk '{print int($3) $4}' | sed 's/\i//g' | sed 's/\,//g')
     [[ ${pv_have} -eq 0 && ${vg_have} -eq 1 ]] && echoerr "  --- " && printf "/ ${C3} "
-    printf "/ ${I1}\n"
+    printf "\n"
 
-    lsblk -b $sd | awk '{print $1}' | sed -r 's#BonusVolGroup-bonusvol([A-Za-z0-9])#\1#g' | sed 1,2d
+    lsblk ${sd} | awk '{print $1}' | sed -r 's#BonusVolGroup-bonusvol([A-Za-z0-9])#\1#g' | sed 1,2d
 done
